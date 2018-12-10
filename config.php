1629f9f2887c0cb7c693e408024f318d7669c059
jhonhansen <42970507+jhonhansen@users.noreply.github.com>
2018-12-10 18:45:36 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+"imei" => "865230020523665",
+"sign" => "3389e2d5af44a4676452bd926b9bc311dbc92891",
+"token" => "1cba147249d76adec554f94b5d5b8946",
+"uuid" => "e777d7fb-2bd1-47f6-911e-435b19e9e584"
+];
